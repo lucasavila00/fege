7359b65c0ee8b9fd01bf9110ba7b25ab151c1d41
Lucas Ávila <lucaxx@gmail.com>
2020-02-16 15:15:56 -0300

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
     title: `Gatsby Typescript Starter`,
   },
   plugins: [
-    `gatsby-plugin-fabric-ui`,
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`,
