6c8f5393feffc80fd91007f57151be691e963774
Lucas Ávila <lucaxx@gmail.com>
2020-02-16 16:33:09 -0300

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-// import Link from 'gatsby-link'
 import { graphql } from "gatsby";
 import { Layout } from "../components/layout";
 import { PrimaryButton } from "office-ui-fabric-react/lib/Button";
 import { Stack } from "office-ui-fabric-react/lib/components/Stack";
 import logo from "./capa.jpg";
+import video1 from "./video1.jpg";
+import play from "./play.png";
 import { OutboundLink } from "gatsby-plugin-google-analytics";
+import { getTheme } from "office-ui-fabric-react/lib/Styling";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -28,19 +30,10 @@ export const pageQuery = graphql`
     }
   }
 `;
-const Header: React.FunctionComponent = () => {
-  return (
-    <img src={logo} style={{ width: "100%" }} />
-    // <div
-    //   style={{
-    //     height: 128 + 64,
-    //     backgroundImage: `url(${logo})`,
-    //     backgroundPosition: "center",
-    //     backgroundSize: "contain",
-    //     backgroundRepeat: "no-repeat",
-    //   }}
-    // />
-  );
+const Image: React.FunctionComponent<{ src: any }> = ({
+  src,
+}) => {
+  return <img src={src} style={{ width: "100%" }} />;
 };
 const Btn: React.FunctionComponent<{
   title: string;
@@ -54,18 +47,72 @@ const Btn: React.FunctionComponent<{
     </OutboundLink>
   );
 };
+const VidBtn: React.FunctionComponent<{
+  title: string;
+  href: string;
+  src: any;
+}> = ({ title, href, src }) => {
+  return (
+    <OutboundLink href={href}>
+      <div
+        style={{
+          fontSize: 0,
+          overflow: "hidden",
+          borderRadius: 2,
+        }}
+      >
+        <div
+          style={{
+            borderColor: getTheme().palette.themePrimary,
+            borderWidth: 4,
+            borderBottomWidth: 0,
+            borderStyle: "solid",
+            minHeight: 128,
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <img
+              src={play}
+              style={{
+                height: 96,
+                width: 96,
+              }}
+            />
+          </div>
+          <Image src={src} />
+        </div>
+        <PrimaryButton
+          style={{ width: "100%", borderRadius: 0 }}
+        >
+          {title}
+        </PrimaryButton>
+      </div>
+    </OutboundLink>
+  );
+};
 
 const Index: React.FunctionComponent<IndexPageProps> = () => {
   return (
     <Layout>
       <Stack tokens={{ childrenGap: "m" }}>
-        <Header />
+        <Image src={logo} />
         <Stack tokens={{ childrenGap: "l1" }}>
           <Btn
             title="Comprar Online"
             href="https://www.instagram.com/festaestranhavicosa/"
           />
-          <Btn
+          <VidBtn
+            src={video1}
             title="Mapa do Evento"
             href="https://www.instagram.com/festaestranhavicosa/"
           />
