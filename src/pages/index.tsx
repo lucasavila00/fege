45010963e69836c6d7494993cf90e60847bc1451
Lucas Ávila <lucaxx@gmail.com>
2020-02-16 14:56:15 -0300

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 // import Link from 'gatsby-link'
 import { graphql } from "gatsby";
 import { Layout } from "../components/layout";
-import { PrimaryButton } from "office-ui-fabric-react/lib/Button"
+import { PrimaryButton } from "office-ui-fabric-react/lib/Button";
 import { Stack } from "office-ui-fabric-react/lib/components/Stack";
-import logo from "./capa.jpg"
+import logo from "./capa.jpg";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -28,22 +28,24 @@ export const pageQuery = graphql`
   }
 `;
 const Header: React.FunctionComponent = () => {
-  return <div
-    style={{
-      height: 128 + 64,
-      backgroundImage: `url(${logo})`,
-      backgroundPosition: "center",
-      backgroundSize: "cover"
-    }}
-  />
-}
+  return (
+    <div
+      style={{
+        height: 128 + 64,
+        backgroundImage: `url(${logo})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    />
+  );
+};
 
 const Index: React.FunctionComponent<IndexPageProps> = () => {
   return (
     <Layout>
       <Stack tokens={{ childrenGap: "m" }}>
-
         <Header />
+        aasdasd
         <Stack tokens={{ childrenGap: "l1" }}>
           <PrimaryButton>Comprar online</PrimaryButton>
           <PrimaryButton>Mapa do Evento</PrimaryButton>
